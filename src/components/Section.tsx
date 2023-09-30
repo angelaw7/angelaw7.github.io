eb9729010577be4814e928cbbc892178ca92b22b
Angela Wang <wangela472@gmail.com>
2023-09-30 11:17:49 -0400

    style: update styling

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-export default function Section({ children }) {
+export default function Section({ children, ...props }: Props) {
   return (
-    <div className="px-36 py-20 bg-orange-200">
+    <div className={"px-36 py-20 " + props.bgColour}>
       <div className="bg-neutral-100 p-10">{children}</div>
     </div>
   );
+}
+
+interface Props {
+  children?: ReactNode;
+  bgColour: string;
 }
